1700bde1b2bf7b71a23be27d109fd898373788d8
Josip Vulic <jovulic@gmail.com>
2026-02-21 18:06:43 -0500

    add(l1): setup terra hardware config

--- l1/hosts/terra/default.nix
+++ l1/hosts/terra/default.nix
@@ -27,11 +27,56 @@ let
     inherit system;
     modules = [
       (
-        { modulesPath, ... }:
+        {
+          config,
+          lib,
+          modulesPath,
+          ...
+        }:
         {
           imports = [
             (modulesPath + "/installer/scan/not-detected.nix")
           ];
+
+          boot = {
+            initrd = {
+              luks.devices.luksroot = {
+                device = "/dev/nvme0n1p3";
+                allowDiscards = true;
+                keyFile = "/dev/nvme0n1p1";
+                keyFileSize = 4096;
+              };
+              availableKernelModules = [
+                "xhci_pci"
+                "ahci"
+                "nvme"
+                "usb_storage"
+                "sd_mod"
+              ];
+              kernelModules = [ "dm-snapshot" ];
+            };
+            kernelModules = [ "kvm-intel" ];
+            extraModulePackages = [ ];
+          };
+
+          fileSystems."/" = {
+            device = "/dev/disk/by-label/root";
+            fsType = "ext4";
+          };
+
+          fileSystems."/boot" = {
+            device = "/dev/disk/by-label/boot";
+            fsType = "vfat";
+            options = [
+              "fmask=0022"
+              "dmask=0022"
+            ];
+          };
+
+          swapDevices = [ ];
+
+          nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+          hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
         }
       )
       ../../modules
