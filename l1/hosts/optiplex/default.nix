4332ffb68e9126013cbc9b508211ef2ddac589c2
Josip Vulic <jovulic@gmail.com>
2026-02-25 12:17:29 -0500

    ftr(l1): add optiplex host

---
+++ l1/hosts/optiplex/default.nix
@@ -0,0 +1,98 @@
+{
+  nixpkgs,
+  deploy-rs,
+  lib,
+  ...
+}:
+let
+  name = "optiplex";
+  userKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDGdXDo+F2+TVAwH3CLJnK2SUIJR/6HvBeHEcfQbYxjk cardno:17_742_648";
+  system = "x86_64-linux";
+  bootstrapNetboot =
+    (nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/netboot/netboot-minimal.nix"
+        ../../bootstrap
+        {
+          bootstrap = {
+            enable = true;
+            device = "/dev/nvme0n1";
+            hostname = name;
+            user.key = userKey;
+          };
+          system.stateVersion = lib.trivial.release;
+        }
+      ];
+    }).config.system.build;
+  targetSystem = nixpkgs.lib.nixosSystem {
+    inherit system;
+    modules = [
+      ../../modules
+      (
+        {
+          config,
+          lib,
+          modulesPath,
+          ...
+        }:
+        {
+          imports = [
+            (modulesPath + "/installer/scan/not-detected.nix")
+          ];
+
+          nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+          hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+          homelab = {
+            boot = {
+              initrd.luksDevice = "/dev/nvme0n1p3";
+              initrd.luksKeyFile = "/dev/nvme0n1p1";
+              initrd.luksKeyFileSize = 4096;
+              initrd.availableKernelModules = [
+                "xhci_pci"
+                "ahci"
+                "nvme"
+                "usbhid"
+              ];
+              initrd.kernelModules = [
+                "dm-snapshot"
+              ];
+              kernelModules = [ "kvm-intel" ];
+            };
+
+            network = {
+              hostName = name;
+              hostAddress = "192.168.1.13";
+              networkInterfaces = [ "enp1s0" ];
+            };
+
+            user.key = userKey;
+          };
+
+          system.stateVersion = "25.11";
+        }
+      )
+    ];
+  };
+in
+{
+  netboot = {
+    hostname = name;
+    mac = "a4:bb:6d:b8:5d:c8";
+    build = bootstrapNetboot;
+  };
+  system = targetSystem;
+  deploy = {
+    nodes = {
+      ${name} = {
+        hostname = "${name}.lan";
+        profiles.system = {
+          sshUser = "root";
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos targetSystem;
+        };
+      };
+    };
+  };
+}
