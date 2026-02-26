5bcef5ff0e5a77d05e26640fee74574eb05e747c
Josip Vulic <jovulic@gmail.com>
2026-02-25 23:24:09 -0500

    add(l1): add certificate proxy and dns to default

--- l1/modules/default.nix
+++ l1/modules/default.nix
@@ -2,11 +2,14 @@
 {
   imports = [
     ./boot.nix
+    ./certificate
+    ./dns.nix
     ./iscsi
     ./monitor.nix
     ./netboot
     ./network.nix
     ./nix.nix
+    ./proxy.nix
     ./registry
     ./tools.nix
     ./user.nix
