2f4e6acbd0e71fd0eca0038a27d39bfd5fe9f36a
Josip Vulic <jovulic@gmail.com>
2026-02-25 23:23:40 -0500

    ftr(l1): create dns module

---
+++ l1/modules/dns.nix
@@ -0,0 +1,48 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.homelab.dns;
+in
+with lib;
+{
+  options.homelab.dns = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable dns.";
+    };
+    records = mkOption {
+      type = types.lines;
+      default = "";
+      description = "DNS records to add to the lab zone, one per line.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    networking.firewall.allowedTCPPorts = [ 53 ];
+    networking.firewall.allowedUDPPorts = [ 53 ];
+
+    environment.etc."coredns/lab.db".text = ''
+      lab. IN SOA ns.lab. admin.lab. 1 86400 7200 4000000 11200
+
+      ${cfg.records}
+    '';
+
+    services.coredns = {
+      enable = true;
+      config = ''
+        .:53 {
+          errors
+          file /etc/coredns/lab.db lab
+          forward . /etc/resolv.conf
+          cache 30
+          loop
+          loadbalance
+        }
+      '';
+    };
+  };
+}
