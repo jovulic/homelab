31b6d60fc2dac182f00ff768d0e87e9de97f2523
Josip Vulic <jovulic@gmail.com>
2026-03-06 21:33:56 -0500

    upd(l1): avoid with usage on pkgs

--- l1/modules/amdgpu.nix
+++ l1/modules/amdgpu.nix
@@ -22,18 +22,18 @@ with lib;
       enable = true;
       enable32Bit = true;
 
-      extraPackages = with pkgs; [
-        rocmPackages.clr.icd # OpenCL support
-        rocmPackages.clr # HIP support
+      extraPackages = [
+        pkgs.rocmPackages.clr.icd # OpenCL support
+        pkgs.rocmPackages.clr # HIP support
       ];
     };
 
     # NOTE: Standard containerd and runc work as amdgpu is open source and
     # baked into the kernel, so no virtualization settings needed.
 
-    environment.systemPackages = with pkgs; [
-      radeontop # top-like tool for viewing amd radeon gpu utilization
-      amdgpu_top # tool to display amdgpu usage
+    environment.systemPackages = [
+      pkgs.radeontop # top-like tool for viewing amd radeon gpu utilization
+      pkgs.amdgpu_top # tool to display amdgpu usage
     ];
   };
 }
